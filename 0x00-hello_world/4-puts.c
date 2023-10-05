0d69890216e5fc934a2166e594cb6ff8a7b7d72f
oumlil <oumlilhlc@gmail.com>
2023-10-05 12:02:10 -0700

    4-puts.cf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Description - Printing lines
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
